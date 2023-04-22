95fdb9570afcffecf5dd607a6f37b0c57e722e6b
CodyMoltar <66773011+CodyMoltar@users.noreply.github.com>
2023-04-22 16:26:08 +0300

    created instrument struct to organize the code

--- main/main.ino
+++ main/main.ino
@@ -1,115 +1,201 @@
-int led = 13;
+#include <Arduino.h>
+#include <Wire.h>
+#include <Adafruit_MCP23017.h>
+#include <Rotary.h>
+#include <RotaryEncOverMCP.h>
 
-int hz = 60;
+// move all pin connections to seperate header file later
+#include <pinConnections.h>
 
-int enA = 10;
-int in1 = 9;
-int in2 = 8;
+#if defined(ESP32) || defined(ESP8266)
+#define INTERRUPT_FUNC_ATTRIB IRAM_ATTR
+#else
+#define INTERRUPT_FUNC_ATTRIB
+#endif
 
-int pot = A0;
+/* Our I2C MCP23017 GPIO expanders */
+Adafruit_MCP23017 mcp;
+
+//Array of pointers of all MCPs if there is more than one
+Adafruit_MCP23017* allMCPs[] = { &mcp };
+constexpr int numMCPs = (int)(sizeof(allMCPs) / sizeof(*allMCPs));
+
+/* the INT pin of the MCP can only be connected to
+ * an interrupt capable pin on the Arduino, either
+ * D3 or D2.
+ * */
+byte arduinoIntPin = 3;
+
+/* variable to indicate that an interrupt has occured */
+volatile boolean awakenByInterrupt = false;
+
+/* function prototypes */
+void intCallBack();
+void cleanInterrupts();
+void handleInterrupt();
+void changeLightFrequency(bool clockwise, int id);
+void changeMotorSpeed(bool clockwise, int id);
+
+/* Array of all rotary encoders and their pins */
+RotaryEncOverMCP rotaryEncoders[] = {
+
+  // instrument 1
+  RotaryEncOverMCP(&mcp, 15, 14, &changeLightFrequency, 0),
+  RotaryEncOverMCP(&mcp, 0, 1, &changeMotorSpeed, 0),
+
+  // instrument 2
+  RotaryEncOverMCP(&mcp, 2, 3, &changeLightFrequency, 1),
+  RotaryEncOverMCP(&mcp, 4, 5, &changeMotorSpeed, 1),
+
+  // instrument 3
+  RotaryEncOverMCP(&mcp, 6, 7, &changeLightFrequency, 2),
+  RotaryEncOverMCP(&mcp, 8, 9, &changeMotorSpeed, 2),
+
+  // instrument 4
+  RotaryEncOverMCP(&mcp, 10, 11, &changeLightFrequency, 3),
+  RotaryEncOverMCP(&mcp, 12, 13, &changeMotorSpeed, 3)
+  
+};
+constexpr int numEncoders = (int)(sizeof(rotaryEncoders) / sizeof(*rotaryEncoders));
+
+
+// add all instruments to the array
+int instruments[4] = {
+	instrument1,
+	instrument2,
+	instrument3,
+	instrument4
+}
+
+// calculate the number of instruments
+int number_of_instruments[sizeof(sensor)];
+
+// set all initial led frequencies and motor speeds
+for(int i = 0; i < number_of_instruments; i++){
+	instruments[i].led_hz = 1;
+	instruments[i].motor_speed_pwm = 0;
+}
+
+void changeLightFrequency(bool clockwise, int id) {
+  
+  if(clockwise && hz[id] < 500){
+    hz[id]++;
+  }
+
+  if(!clockwise && speed[id] > 1) {
+    speed[id]--;
+  }
+
+
+}
+
+void changeMotorSpeed(bool clockwise, int id) {
+  
+  if(clockwise && speed[id] < 255){
+
+    speed[id]++;
+    analogWrite(instruments[id][3], speed[id]);
+
+  }
+
+  if(!clockwise && speed[id] > 0) {
+    speed[id]--;
+    analogWrite(instruments[id][3], speed[id]);
+  }
+
+}
+
+// constants won't change. Used here to set a pin number:
+const int ledPin = LED_BUILTIN;  // the number of the LED pin
+
+// Variables will change:
+int ledState = LOW;  // ledState used to set the LED
+
+// Generally, you should use "unsigned long" for variables that hold time
+// The value will quickly become too large for an int to store
+unsigned long previousMillis = 0;  // will store last time LED was updated
+
+// constants won't change:
+
 
 void setup() {
-  // put your setup code here, to run once:
-  pinMode(led, OUTPUT);
-  digitalWrite(led, LOW);
 
-  pinMode(enA, OUTPUT);
-	pinMode(in1, OUTPUT);
-	pinMode(in2, OUTPUT);
-	
-	// Turn off motors - Initial state
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
+  pinMode(ledPin, OUTPUT);
 
-  pinMode(A0, INPUT);
-  Serial.begin(9600);
 
-  digitalWrite(in1, LOW);
-	digitalWrite(in2, HIGH);
-  analogWrite(enA, 30);
+  // Serial.begin(115200);
+  // Serial.println("MCP23007 Interrupt Test");
 
+  pinMode(arduinoIntPin, INPUT);
+
+  mcp.begin();         // use default address 0
+  mcp.readINTCAPAB();  //read this so that the interrupt is cleared
+
+  //initialize all rotary encoders
+
+  //Setup interrupts, OR INTA, INTB together on both ports.
+  //thus we will receive an interrupt if something happened on
+  //port A or B with only a single INT connection.
+  mcp.setupInterrupts(true, false, LOW);
+
+  //Initialize input encoders (pin mode, interrupt)
+  for (int i = 0; i < numEncoders; i++) {
+    rotaryEncoders[i].init();
+  }
+
+  attachInterrupt(digitalPinToInterrupt(arduinoIntPin), intCallBack, FALLING);
+}
+
+// The int handler will just signal that the int has happened
+// we will do the work from the main loop.
+void INTERRUPT_FUNC_ATTRIB intCallBack() {
+  awakenByInterrupt = true;
+}
+
+void checkInterrupt() {
+  if (awakenByInterrupt) {
+    // disable interrupts while handling them.
+    detachInterrupt(digitalPinToInterrupt(arduinoIntPin));
+    handleInterrupt();
+    attachInterrupt(digitalPinToInterrupt(arduinoIntPin), intCallBack, FALLING);
+  }
+}
+
+void handleInterrupt() {
+  //Read the entire state when the interrupt occurred
+
+  //An interrupt occurred on some MCP object.
+  //since all of them are ORed together, we don't
+  //know exactly which one has fired.
+  //just read all of them, pre-emptively.
+
+  for (int j = 0; j < numMCPs; j++) {
+    uint16_t gpioAB = allMCPs[j]->readINTCAPAB();
+    // we need to read GPIOAB to clear the interrupt actually.
+    volatile uint16_t dummy = allMCPs[j]->readGPIOAB();
+    for (int i = 0; i < numEncoders; i++) {
+      //only feed this in the encoder if this
+      //is coming from the correct MCP
+      if (rotaryEncoders[i].getMCP() == allMCPs[j])
+        rotaryEncoders[i].feedInput(gpioAB);
+    }
+  }
+
+  cleanInterrupts();
+}
+
+// handy for interrupts triggered by buttons
+// normally signal a few due to bouncing issues
+void cleanInterrupts() {
+#ifdef __AVR__
+  EIFR = 0x01;
+#endif
+  awakenByInterrupt = false;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
-  // strobe();
-
-  // float reading = analogRead(pot);
-  // float value = mapfloat(reading, 0, 1023, 0, 255);
-  // Serial.print("original reading: "); 
-  // Serial.print(reading);
-  // Serial.print(" and mapped value: ");
-  // Serial.println(value);
-
-  // analogWrite(enA, value);
-
-  // digitalWrite(led, HIGH);
-  // delay(10);
-  // digitalWrite(led, LOW);
-  // delay(10);  
 
 
-}
-
-float mapfloat(float x, float in_min, float in_max, float out_min, float out_max)
-{
-  return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min;
-}
-
-void directionControl() {
-	// Set motors to maximum speed
-	// For PWM maximum possible values are 0 to 255
-	analogWrite(enA, 255);
-
-	// Turn on motor A & B
-	digitalWrite(in1, HIGH);
-	digitalWrite(in2, LOW);
-	delay(2000);
-	
-	// Now change motor directions
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, HIGH);
-	delay(2000);
-	
-	// Turn off motors
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
-}
-
-void speedControl() {
-	// Turn on motors
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, HIGH);
-	
-	// Accelerate from zero to maximum speed
-	for (int i = 0; i < 256; i++) {
-		analogWrite(enA, i);
-		delay(20);
-	}
-	
-	// Decelerate from maximum speed to zero
-	for (int i = 255; i >= 0; --i) {
-		analogWrite(enA, i);
-		delay(20);
-	}
-	
-	// Now turn off motors
-	digitalWrite(in1, LOW);
-	digitalWrite(in2, LOW);
-}
-
-void strobe(){
-  if(hz > 1000){
-    hz = 1;
-  }
-
-  int time = 1000/hz;
-
-  digitalWrite(led, HIGH);
-  delay(time);
-  digitalWrite(led, LOW);
-  delay(time);
-
-  hz++;
+  //Check if an interrupt has occurred and act on it
+  checkInterrupt();
 }
---
+++ main/pinConnections.h
@@ -0,0 +1,96 @@
+// // pin connected to the mosfet controlling LED
+// #define instrument_1_LED 0
+
+// // de gpios used on the mcp23017 for the encoders controlling the light frequency
+// #define instrument_1_LED_rotary_A 0
+// #define instrument_1_LED_rotary_B 1
+
+// // pins connected to the L298N motor controller
+// #define instrument_1_motor_A 2
+// #define instrument_1_motor_B 4
+// #define instrument_1_motor_enable A0
+
+// // de gpios used on the mcp23017 for the encoders controlling the motor speed
+// #define instrument_1_motor_rotary_A 2
+// #define instrument_1_motor_rotary_B 3
+
+// // pins connected to the 7segment display to show both the motor speed and light frequency
+// #define instrument_1_display_din 5
+// #define instrument_1_display_clock 6
+// #define instrument_1_display_cs 7
+
+struct instrument{
+    int LED;
+    int LED_rotary_A;
+    int LED_rotary_B;
+
+    int motor_A;
+    int motor_B;
+    int motor_enable;
+
+    int motor_rotary_A;
+    int motor_rotary_B;
+
+    int display_din;
+    int display_clock;
+    int display_cs;
+
+    int led_hz;
+    int motor_speed_pwm;
+}
+
+instrument instrument1 = {
+    0,  // LED
+    0,  // LED encoder A
+    1,  // LED encoder B
+    1,  // motor controller A
+    2,  // motor controller B
+    A0, // motor controller enable
+    2,  // motor encoder A
+    3,  // motor encoder B
+    4,  // 7-segment-display din
+    5,  // 7-segment-display clock
+    6   // 7-segment-display cs
+}
+
+instrument instrument2 = {
+    7,  // LED
+    4,  // LED encoder A
+    5,  // LED encoder B
+    8,  // motor controller A
+    9,  // motor controller B
+    A1, // motor controller enable
+    6,  // motor encoder A
+    7,  // motor encoder B
+    10, // 7-segment-display din
+    11, // 7-segment-display clock
+    12  // 7-segment-display cs
+}
+
+instrument instrument3 = {
+    13, // LED
+    8,  // LED encoder A
+    9,  // LED encoder B
+    14, // motor controller A
+    15, // motor controller B
+    A2, // motor controller enable
+    10, // motor encoder A
+    11, // motor encoder B
+    16, // 7-segment-display din
+    17, // 7-segment-display clock
+    18  // 7-segment-display cs
+}
+
+instrument instrument4 = {
+    19, // LED
+    12, // LED encoder A
+    13, // LED encoder B
+    20, // motor controller A
+    21, // motor controller B
+    A3, // motor controller enable
+    14, // motor encoder A
+    15, // motor encoder B
+    22, // 7-segment-display din
+    23, // 7-segment-display clock
+    24  // 7-segment-display cs
+}
