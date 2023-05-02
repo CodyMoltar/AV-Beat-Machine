db455375bfaca238633c5da48bf1a9b4f5e87fe7
CodyMoltar <66773011+CodyMoltar@users.noreply.github.com>
2023-05-02 09:01:22 +0300

    first instrument working

--- tests/4x7segmentdisplay/4x7segmentdisplay.ino
+++ tests/4x7segmentdisplay/4x7segmentdisplay.ino
@@ -8,11 +8,8 @@
  pin 11 is connected to the CLK 
  pin 10 is connected to LOAD 
  We have only a single MAX72XX.
- */ 
-
- // DATA IN, CLOCK, CS
-LedControl lc1=LedControl(7,8,9,2);
-
+ */
+LedControl lc=LedControl(8,9,10,1);
 
 /* we always wait a bit between updates of the display */
 unsigned long delaytime=250;
@@ -22,23 +19,11 @@ void setup() {
    The MAX72XX is in power-saving mode on startup,
    we have to do a wakeup call
    */
-  lc1.shutdown(0,false);
+  lc.shutdown(0,false);
   /* Set the brightness to a medium values */
-  lc1.setIntensity(0,0);
+  lc.setIntensity(0,0);
   /* and clear the display */
-  lc1.clearDisplay(0);
-
-  lc1.shutdown(1,false);
-  /* Set the brightness to a medium values */
-  lc1.setIntensity(1,0);
-  /* and clear the display */
-  lc1.clearDisplay(1);
-
-  // lc2.shutdown(0,false);
-  /* Set the brightness to a medium values */
-  // lc2.setIntensity(0,8);
-  /* and clear the display */
-  // lc2.clearDisplay(0);
+  lc.clearDisplay(0);
 }
 
 
@@ -46,74 +31,43 @@ void setup() {
  This method will display the characters for the
  word "Arduino" one after the other on digit 0. 
  */
-// void writeArduinoOn7Segment() {
-//   lc.setChar(0,0,'a',false);
-//   delay(delaytime);
-//   lc.setRow(0,0,0x05);
-//   delay(delaytime);
-//   lc.setChar(0,0,'d',false);
-//   delay(delaytime);
-//   lc.setRow(0,0,0x1c);
-//   delay(delaytime);
-//   lc.setRow(0,0,B00010000);
-//   delay(delaytime);
-//   lc.setRow(0,0,0x15);
-//   delay(delaytime);
-//   lc.setRow(0,0,0x1D);
-//   delay(delaytime);
-//   lc.clearDisplay(0);
-//   delay(delaytime);
-// } 
+void writeArduinoOn7Segment() {
+  lc.setChar(0,0,'a',false);
+  delay(delaytime);
+  lc.setRow(0,0,0x05);
+  delay(delaytime);
+  lc.setChar(0,0,'d',false);
+  delay(delaytime);
+  lc.setRow(0,0,0x1c);
+  delay(delaytime);
+  lc.setRow(0,0,B00010000);
+  delay(delaytime);
+  lc.setRow(0,0,0x15);
+  delay(delaytime);
+  lc.setRow(0,0,0x1D);
+  delay(delaytime);
+  lc.clearDisplay(0);
+  delay(delaytime);
+} 
 
-// /*
-//   This method will scroll all the hexa-decimal
-//  numbers and letters on the display. You will need at least
-//  four 7-Segment digits. otherwise it won't really look that good.
-//  */
-// void scrollDigits() {
-//   for(int i=0;i<13;i++) {
-//     lc.setDigit(0,3,i,false);
-//     lc.setDigit(0,2,i+1,false);
-//     lc.setDigit(0,1,i+2,false);
-//     lc.setDigit(0,0,i+3,false);
-//     delay(delaytime);
-//   }
-//   lc.clearDisplay(0);
-//   delay(delaytime);
-// }
-
-int count = 0;
+/*
+  This method will scroll all the hexa-decimal
+ numbers and letters on the display. You will need at least
+ four 7-Segment digits. otherwise it won't really look that good.
+ */
+void scrollDigits() {
+  for(int i=0;i<13;i++) {
+    lc.setDigit(0,3,i,false);
+    lc.setDigit(0,2,i+1,false);
+    lc.setDigit(0,1,i+2,false);
+    lc.setDigit(0,0,i+3,false);
+    delay(delaytime);
+  }
+  lc.clearDisplay(0);
+  delay(delaytime);
+}
 
 void loop() { 
-  // writeArduinoOn7Segment();
-  // scrollDigits();
-
-  int ones;
-  int ones;
-
-  
-  lc1.setDigit(0,0,9,false);
-  lc1.setDigit(0,1,9,false);
-  lc1.setDigit(0,2,9,false);
-  lc1.setDigit(0,3,9,false);
-  lc1.setDigit(0,4,9,false);
-  lc1.setDigit(0,5,9,false);
-  lc1.setDigit(0,6,9,false);
-  lc1.setDigit(0,7,9,false);
-  delay(1000);
-  lc1.clearDisplay(0);
-  lc1.setDigit(1,0,9,false);
-  lc1.setDigit(1,1,9,false);
-  lc1.setDigit(1,2,9,false);
-  lc1.setDigit(1,3,9,false);
-  lc1.setDigit(1,4,9,false);
-  lc1.setDigit(1,5,9,false);
-  lc1.setDigit(1,6,9,false);
-  lc1.setDigit(1,7,9,false);
-  delay(1000);
-  lc1.clearDisplay(1);
-
-  // lc2.setDigit(0,3,7,false);
-
-  
+  writeArduinoOn7Segment();
+  scrollDigits();
 }
--- tests/single_encoder/single_encoder.ino
+++ tests/single_encoder/single_encoder.ino
@@ -1,224 +1,21 @@
-// #include <Arduino.h>
-// #include <Wire.h>
-// #include <Adafruit_MCP23017.h>
-// #include <Rotary.h>
-// #include <RotaryEncOverMCP.h>
-
-// int hz = 400;
-// int motorspeed = 60;
-
-// int motorA = 22;
-// int motorB = 24;
-// int motorE = 5;
-
-// const int ledPin = 13;  // the number of the LED pin
-
-// // Variables will change:
-// int ledState = LOW;  // ledState used to set the LED
-
-// // Generally, you should use "unsigned long" for variables that hold time
-// // The value will quickly become too large for an int to store
-// unsigned long previousMillis = 0;  // will store last time LED was updated
-
-// #if defined(ESP32) || defined(ESP8266)
-// #define INTERRUPT_FUNC_ATTRIB IRAM_ATTR
-// #else
-// #define INTERRUPT_FUNC_ATTRIB  
-// #endif
-
-// /* Our I2C MCP23017 GPIO expanders */
-// Adafruit_MCP23017 mcp;
-
-// //Array of pointers of all MCPs if there is more than one
-// Adafruit_MCP23017* allMCPs[] = { &mcp };
-// constexpr int numMCPs = (int)(sizeof(allMCPs) / sizeof(*allMCPs));
-
-// /* the INT pin of the MCP can only be connected to
-//  * an interrupt capable pin on the Arduino, either
-//  * D3 or D2.
-//  * */
-// byte arduinoIntPin = 3;
-
-// /* variable to indicate that an interrupt has occured */
-// volatile boolean awakenByInterrupt = false;
-
-// /* function prototypes */
-// void intCallBack();
-// void cleanInterrupts();
-// void handleInterrupt();
-// void RotaryEncoderChanged(bool clockwise, int id);
-// void changeLight(bool clockwise, int id);
-// void changeMotor(bool clockwise, int id);
-
-// /* Array of all rotary encoders and their pins */
-// RotaryEncOverMCP rotaryEncoders[] = {
-//     // outputA,B on GPB7,GPB6, register with callback and ID=1 (GPB0=7 .. GPB7=15)
-//     // RotaryEncOverMCP(&mcp, 7, 6, &changeMotor, 0),
-//     RotaryEncOverMCP(&mcp, 5, 4, &changeLight, 0)
-// };
-// constexpr int numEncoders = (int)(sizeof(rotaryEncoders) / sizeof(*rotaryEncoders));
-
-// void RotaryEncoderChanged(bool clockwise, int id) {
-//     Serial.println("Encoder " + String(id) + ": "
-//             + (clockwise ? String("clockwise") : String("counter-clock-wise")));
-// }
-
-// void changeLight(bool clockwise, int id) {
-
-//   if(clockwise){
-//     if(hz < 1000){
-//       hz++;
-//     }
-//   }
-//   else{
-//     if(hz > 0){
-//       hz--;
-//     }
-//   }
-
-//   Serial.println("Light Hz: " + String(hz) + "     Motorspeed: " + String(motorspeed));
-  
-// }
-
-// void changeMotor(bool clockwise, int id) {
-
-//   if(clockwise){
-//     if(motorspeed < 255){
-//       motorspeed++;
-//     }
-//   }
-//   else{
-//     if(motorspeed > 0){
-//       motorspeed--;
-//     }
-//   }
-
-//   analogWrite(motorE, motorspeed);
-
-//   Serial.println("Light Hz: " + String(hz) + "     Motorspeed: " + String(motorspeed));
-  
-// }
-
-
-
-// void setup(){
-
-//     Serial.begin(115200);
-//     Serial.println("MCP23007 Interrupt Test");
-
-//     pinMode(arduinoIntPin,INPUT);
-
-//     pinMode(ledPin, OUTPUT);
-
-//     mcp.begin();      // use default address 0
-//     mcp.readINTCAPAB(); //read this so that the interrupt is cleared
-
-//     //initialize all rotary encoders
-
-//     pinMode(motorA, OUTPUT);
-//     pinMode(motorB, OUTPUT);
-//     digitalWrite(motorA, HIGH);
-//     digitalWrite(motorB, LOW);
-//     analogWrite(motorE, motorspeed);
-
-//     //Setup interrupts, OR INTA, INTB together on both ports.
-//     //thus we will receive an interrupt if something happened on
-//     //port A or B with only a single INT connection.
-//     mcp.setupInterrupts(true,false,LOW);
-
-//     //Initialize input encoders (pin mode, interrupt)
-//     for(int i=0; i < numEncoders; i++) {
-//         rotaryEncoders[i].init();
-//     }
-
-//     attachInterrupt(digitalPinToInterrupt(arduinoIntPin), intCallBack, FALLING);
-// }
-
-// // The int handler will just signal that the int has happened
-// // we will do the work from the main loop.
-// void INTERRUPT_FUNC_ATTRIB intCallBack() {
-//     awakenByInterrupt=true;
-// }
-
-// void checkInterrupt() {
-//     if(awakenByInterrupt) {
-//         // disable interrupts while handling them.
-//         detachInterrupt(digitalPinToInterrupt(arduinoIntPin));
-//         handleInterrupt();
-//         attachInterrupt(digitalPinToInterrupt(arduinoIntPin),intCallBack,FALLING);
-//     }
-// }
-
-// void handleInterrupt(){
-//     //Read the entire state when the interrupt occurred
-
-//     //An interrupt occurred on some MCP object.
-//     //since all of them are ORed together, we don't
-//     //know exactly which one has fired.
-//     //just read all of them, pre-emptively.
-
-//     for(int j = 0; j < numMCPs; j++) {
-//         uint16_t gpioAB = allMCPs[j]->readINTCAPAB();
-//         // we need to read GPIOAB to clear the interrupt actually.
-//         volatile uint16_t dummy = allMCPs[j]->readGPIOAB();
-//         for (int i=0; i < numEncoders; i++) {
-//             //only feed this in the encoder if this
-//             //is coming from the correct MCP
-//             if(rotaryEncoders[i].getMCP() == allMCPs[j])
-//                 rotaryEncoders[i].feedInput(gpioAB);
-//         }
-//     }
-
-//     cleanInterrupts();
-// }
-
-// // handy for interrupts triggered by buttons
-// // normally signal a few due to bouncing issues
-// void cleanInterrupts(){
-// #ifdef __AVR__
-//     EIFR=0x01;
-// #endif
-//     awakenByInterrupt=false;
-// }
-
-// void loop() {
-//   //Check if an interrupt has occurred and act on it
-//   checkInterrupt();
-
-//   unsigned long currentMillis = millis();
-
-//   if (currentMillis - previousMillis >= 1000/hz) {
-//     // save the last time you blinked the LED
-//     previousMillis = currentMillis;
-
-//     // if the LED is off turn it on and vice-versa:
-//     if (ledState == LOW) {
-//       ledState = HIGH;
-//     } else {
-//       ledState = LOW;
-//     }
-
-//     // set the LED with the ledState of the variable:
-//     digitalWrite(ledPin, ledState);
-//   }
-
-// }
-
-
 #include <Arduino.h>
 #include <Wire.h>
 #include <Adafruit_MCP23017.h>
 #include <Rotary.h>
 #include <RotaryEncOverMCP.h>
 
-int hz = 400;
+#include "LedControl.h"
+
+LedControl lc=LedControl(8,9,10,1);
+
+int hz = 35;
 int motorspeed = 0;
 
 int motorA = 22;
 int motorB = 24;
 int motorE = 5;
 
-const int ledPin = 13;  // the number of the LED pin
+const int ledPin = 12;  // the number of the LED pin
 
 // Variables will change:
 int ledState = LOW;  // ledState used to set the LED
@@ -270,16 +67,52 @@ void RotaryEncoderChanged(bool clockwise, int id) {
             + (clockwise ? String("clockwise") : String("counter-clock-wise")));
 }
 
+int updateLightNumber(int value){
+
+  int thousands = value%10000;
+  int hundreds = value%1000;
+  int tens = value%100;
+  int ones = value%10;
+
+  int seperatedValues[4] = {thousands, hundreds, tens, ones};
+
+  return seperatedValues;
+}
+
 void changeLight(bool clockwise, int id) {
 
   if(clockwise){
     if(hz < 1000){
       hz++;
+      lc.clearDisplay(0);
+      lc.setDigit(0,3,(hz/1000)%10,false);
+      lc.setDigit(0,2,(hz/100)%10,false);
+      lc.setDigit(0,1,(hz/10)%10,false);
+      lc.setDigit(0,0,hz%10,false);
+      lc.setDigit(0,3,(hz/1000)%10,false);
+      lc.setDigit(0,2,(hz/100)%10,false);
+      lc.setDigit(0,1,(hz/10)%10,false);
+      lc.setDigit(0,0,hz%10,false);
+      lc.setDigit(0,7,(motorspeed/1000)%10,false);
+      lc.setDigit(0,6,(motorspeed/100)%10,false);
+      lc.setDigit(0,5,(motorspeed/10)%10,false);
+      lc.setDigit(0,4,motorspeed%10,false);
+      tone(ledPin, hz);
     }
   }
   else{
     if(hz > 0){
       hz--;
+      lc.clearDisplay(0);
+      lc.setDigit(0,3,(hz/1000)%10,false);
+      lc.setDigit(0,2,(hz/100)%10,false);
+      lc.setDigit(0,1,(hz/10)%10,false);
+      lc.setDigit(0,0,hz%10,false);
+      lc.setDigit(0,7,(motorspeed/1000)%10,false);
+      lc.setDigit(0,6,(motorspeed/100)%10,false);
+      lc.setDigit(0,5,(motorspeed/10)%10,false);
+      lc.setDigit(0,4,motorspeed%10,false);
+      tone(ledPin, hz);
     }
   }
 
@@ -292,15 +125,35 @@ void changeMotor(bool clockwise, int id) {
   if(clockwise){
     if(motorspeed < 255){
       motorspeed++;
+      lc.clearDisplay(0);
+      lc.setDigit(0,3,(hz/1000)%10,false);
+      lc.setDigit(0,2,(hz/100)%10,false);
+      lc.setDigit(0,1,(hz/10)%10,false);
+      lc.setDigit(0,0,hz%10,false);
+      lc.setDigit(0,7,(motorspeed/1000)%10,false);
+      lc.setDigit(0,6,(motorspeed/100)%10,false);
+      lc.setDigit(0,5,(motorspeed/10)%10,false);
+      lc.setDigit(0,4,motorspeed%10,false);
+      analogWrite(motorE, motorspeed);
     }
   }
   else{
     if(motorspeed > 0){
       motorspeed--;
+      lc.clearDisplay(0);
+      lc.setDigit(0,3,(hz/1000)%10,false);
+      lc.setDigit(0,2,(hz/100)%10,false);
+      lc.setDigit(0,1,(hz/10)%10,false);
+      lc.setDigit(0,0,hz%10,false);
+      lc.setDigit(0,7,(motorspeed/1000)%10,false);
+      lc.setDigit(0,6,(motorspeed/100)%10,false);
+      lc.setDigit(0,5,(motorspeed/10)%10,false);
+      lc.setDigit(0,4,motorspeed%10,false);
+      analogWrite(motorE, motorspeed);
     }
   }
 
-  analogWrite(motorE, motorspeed);
+  
 
   Serial.println("Light Hz: " + String(hz) + "     Motorspeed: " + String(motorspeed));
   
@@ -311,7 +164,22 @@ void changeMotor(bool clockwise, int id) {
 void setup(){
 
     Serial.begin(115200);
-    Serial.println("MCP23007 Interrupt Test");
+    Serial.println("Starting Audio Visual Beat machine");
+
+    lc.shutdown(0,false);
+    /* Set the brightness to a medium values */
+    lc.setIntensity(0,8);
+    /* and clear the display */
+    lc.clearDisplay(0);
+
+    lc.setDigit(0,3,(hz/1000)%10,false);
+    lc.setDigit(0,2,(hz/100)%10,false);
+    lc.setDigit(0,1,(hz/10)%10,false);
+    lc.setDigit(0,0,hz%10,false);
+    lc.setDigit(0,7,(motorspeed/1000)%10,false);
+    lc.setDigit(0,6,(motorspeed/100)%10,false);
+    lc.setDigit(0,5,(motorspeed/10)%10,false);
+    lc.setDigit(0,4,motorspeed%10,false);
 
     pinMode(arduinoIntPin,INPUT);
 
@@ -327,6 +195,8 @@ void setup(){
     digitalWrite(motorA, HIGH);
     digitalWrite(motorB, LOW);
     analogWrite(motorE, motorspeed);
+
+    tone(ledPin, hz); 
 
     //Setup interrupts, OR INTA, INTB together on both ports.
     //thus we will receive an interrupt if something happened on
@@ -391,22 +261,5 @@ void cleanInterrupts(){
 void loop() {
   //Check if an interrupt has occurred and act on it
   checkInterrupt();
-
-  unsigned long currentMillis = micros();
-
-  if (currentMillis - previousMillis >= 1000/hz) {
-    // save the last time you blinked the LED
-    previousMillis = currentM illis;
-
-    // if the LED is off turn it on and vice-versa:
-    if (ledState == LOW) {
-      ledState = HIGH;
-    } else {
-      ledState = LOW;
-    }
-
-    // set the LED with the ledState of the variable:
-    digitalWrite(ledPin, ledState);
-  }
 
 }
